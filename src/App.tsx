24ed648c54d3735a1e07a3638e130792dbcf48e3
Yuliia <uliagrinenko1981@gmail.com>
2025-05-19 22:34:59 +0200

    correkt

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 
 //import Homework08 from './homework/homework08/Homework08'
-import Lesson09 from './lessons/lesson09/Lesson09'
+//import Lesson09 from './lessons/lesson09/Lesson09'
 import './App.css'
 import Lesson10 from './lessons/lesson10/Lesson10'
 
